6ce717837d1208f6f8741f6b5f611933bc8f2cfc
Dinesh <flexdinesh@gmail.com>
2018-02-22 23:37:37 +0530

    Init Project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-unused-vars': 'off',
+    'no-return-assign': 'off',
+    'global-require': 'off',
+    'import/no-dynamic-require': 'off',
+    'comma-dangle': ['off'],
+  },
+};
